d8ce339b4fac3a0cd5f912b16b21cd862e293bd5
dhargan <teomanunay@gmail.com>
2022-10-16 23:38:31 +0300

    Create and use Header component

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import logo from "../images/logo.png";
+
+const Header = (props) => {
+    return (
+        <div className="flex items-center justify-center text-center h-40 bg-light-purple">
+            <div className="flex-col">
+                <img
+                    className="shadow rounded-full border-2 border-yellow shadow-black"
+                    src={logo}
+                    alt="logo"
+                />
+            </div>
+            <div className="flex-col ml-4">
+                <div className="flex-row mb-2 mt-3">
+                    <span className="text-5xl">
+                        <span className="font-thin text-white">A. TEOMAN</span>
+                        <span className="font-bold text-yellow"> UNAY</span>
+                    </span>
+                </div>
+                <div className="flex-row">
+                    <span className="font-bold text-white text-4xl">
+                        Software Engineer
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
