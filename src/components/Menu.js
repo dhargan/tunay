1c0de190f249dd569a759557a71e289a65641c8e
dhargan <teomanunay@gmail.com>
2022-10-16 23:58:49 +0300

    Prepare static menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,14 @@
 const Menu = (props) => {
     return (
-        <div className="flex items-center justify-center text-center h-12 bg-gray">
-            menu
+        <div class="flex items-center justify-center text-center h-12 bg-gray">
+            <div class="contents">
+                <div class="flex-1 text-dark-purple text-2xl font-bold">
+                    ABOUT
+                </div>
+                <div class="flex-1 text-dark-purple text-2xl">EDUCATION</div>
+                <div class="flex-1 text-dark-purple text-2xl">WORK</div>
+                <div class="flex-1 text-dark-purple text-2xl">HOBBIES</div>
+            </div>
         </div>
     );
 };
